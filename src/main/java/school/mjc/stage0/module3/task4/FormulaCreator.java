f50966ec1cdda5632e5a04fcdc16c6a659f2005e
nodirbekcs <fayzullayevnodir0@gmail.com>
2023-06-22 16:56:01 +0500

    great

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        System.out.println((9+"a2"+5+"b"+2+"a"+"-"+7)+"*"+(("a"+"b"+"-"+4+"ab")+"/"+2));
     }
 }
